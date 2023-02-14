4a1a9a0d3f0dfb0eabac4f3b28482f11cc7636aa
muntaswir <muntaswir.mw@gmail.com>
2023-02-14 18:32:36 +0400

    fix path

---
+++ src/components/invoice.js
@@ -0,0 +1,9 @@
+import{Box, Typography, useTheme}from "@mui/material";
+import {DataGrid} from "@mui/x-data-grid"
+
+const invoice = () => {
+
+    return ( <Typography variant="h1">hello world</Typography> );
+}
+ 
+export default invoice;
