981ea94dd70654521e7aed80ec851c5ea0245d0d
muntaswir <muntaswir.mw@gmail.com>
2023-02-14 16:58:55 +0400

    path built

---
+++ src/components/test.js
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+const table = () => {
+    return (
+    <Typography variant="h1">This is the Invoice page</Typography>
+    );
+}
+ 
+export default table;
